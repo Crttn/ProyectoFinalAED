b5fd49e59af10ec3dc61e751c963de767f5e2824
Cristian <134507343+crisPerez3400@users.noreply.github.com>
2025-02-05 13:40:14 +0000

    Agregado añadir en Proveedores

--- src/main/java/es/crttn/dad/controllers/ProveedoresController.java
+++ src/main/java/es/crttn/dad/controllers/ProveedoresController.java
@@ -1,17 +1,29 @@
 package es.crttn.dad.controllers;
 
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 import es.crttn.dad.App;
+import es.crttn.dad.DatabaseConector;
 import es.crttn.dad.modelos.Producto;
+import es.crttn.dad.modelos.Proveedor;
+import javafx.application.Platform;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import org.bson.types.ObjectId;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.ResourceBundle;
 
@@ -26,37 +38,65 @@ public class ProveedoresController implements Initializable {
             throw new RuntimeException(e);
         }
     }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        idColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getId().toHexString()));
+        nombreColumn.setCellValueFactory(new PropertyValueFactory<>("nombre"));
+        direccionColumn.setCellValueFactory(new PropertyValueFactory<>("direccion"));
+        contactoColumn.setCellValueFactory(new PropertyValueFactory<>("contacto"));
+
+        proveedoresList = FXCollections.observableArrayList();
+        proveedoresTableView.setItems(proveedoresList);
+
+        showData();
 
     }
 
+
+    private void showData() {
+        MongoDatabase db = DatabaseConector.getInstance().getDatabase();
+        MongoCollection<Proveedor> collection = db.getCollection("proveedores", Proveedor.class);
+        List<Proveedor> proveedores = collection.find().into(new ArrayList<>());
+
+        for (Proveedor p : proveedores) {
+            System.out.println("ID: " + p.getId().toHexString());
+            System.out.println("nombre: " + p.getNombre());
+            System.out.println("Dirección: " + p.getDireccion());
+            System.out.println("Contacto: " + p.getContacto());
+            System.out.println("-----------------------------");
+        }
+
+        Platform.runLater(() -> {
+            proveedoresList.setAll(proveedores);
+        });
+    }
 
     public BorderPane getRoot() {
         return root;
     }
 
     @FXML
-    private TableColumn<?, ?> contactoColumn;
+    private TableView<Proveedor> proveedoresTableView;
 
     @FXML
-    private TableColumn<?, ?> direccionColumn;
+    private TableColumn<Proveedor, String> contactoColumn;
 
     @FXML
-    private TableColumn<?, ?> idColumn;
+    private TableColumn<Proveedor, String> direccionColumn;
 
     @FXML
-    private TableColumn<?, ?> nombreColumn;
+    private TableColumn<Proveedor, String> idColumn;
 
     @FXML
-    private TableView<?> proveedoresTableView;
+    private TableColumn<Proveedor, String> nombreColumn;
 
     @FXML
     private BorderPane root;
 
     @FXML
     void onAgregarAction(ActionEvent event) {
-        Dialog<Producto> dialog = new Dialog<>();
+        Dialog<Proveedor> dialog = new Dialog<>();
         dialog.setTitle("Agregar Proveedor");
         dialog.setHeaderText("Ingrese los datos del nuevo proveedor");
 
@@ -80,11 +120,53 @@ public class ProveedoresController implements Initializable {
 
         dialog.getDialogPane().setContent(grid);
 
-        Optional<Producto> result = dialog.showAndWait();
+        dialog.setResultConverter(dialogButton -> {
+            if (dialogButton == addButtonType) {
+                try {
+                    String nombre = nombreField.getText();
+                    String direccion = direccionField.getText();
+                    String contacto = contactoField.getText();
+
+                    if (nombre.isEmpty() || direccion.isEmpty() || contacto.isEmpty()) {
+                        // mostrarAlerta("Error", "Todos los campos son obligatorios.");
+                        return null;
+                    }
+
+                    Proveedor nuevoProveedor = new Proveedor(nombre, direccion, contacto);
+                    return nuevoProveedor;
+                } catch (NumberFormatException e) {
+                    return null;
+                }
 
 
+            }
+
+            return null;
+        });
+
+
+        Optional<Proveedor> result = dialog.showAndWait();
+
+        result.ifPresent(this::agregarProveedorABaseDeDatos);
 
     }
+
+    private void agregarProveedorABaseDeDatos(Proveedor proveedor) {
+        MongoDatabase db = DatabaseConector.getInstance().getDatabase();
+        MongoCollection<Proveedor> collection = db.getCollection("proveedores", Proveedor.class);
+
+        try {
+            collection.insertOne(proveedor);
+            proveedoresList.add(proveedor); // Agregar a la lista observable
+            proveedoresTableView.refresh();
+        } catch (Exception e) {
+            // mostrarAlerta("Error", "No se pudo agregar el producto.");
+            e.printStackTrace();
+        }
+    }
+
+
+    ObservableList proveedoresList;
 
     @FXML
     void onBackAction(ActionEvent event) {
